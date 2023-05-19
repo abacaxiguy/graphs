ef15b99b193a40a54827b1d731433d010d0ac370
João Lucas <jlucasoc@hotmail.com>
2023-05-19 14:07:55 -0300

    Melhorando a performance do `dijkstra`
    Co-Authored-By: Mateus <73254705+mateussf99@users.noreply.github.com>

--- dijkstra/dijkstra.cpp
+++ dijkstra/dijkstra.cpp
@@ -21,9 +21,9 @@ public:
     // construtor
     Grafo(int v)
     {
-        this->v = v + 1; // atribuindo o numero de vertices
+        this->v = v; // atribuindo o numero de vertices
         /* cria as lista on de cada lista é uma lista de pairs onde cada pair é formado pelo vertice de destino e o custo*/
-        adj = new list<pair<int, int>>[v + 1]; // eu tenho v listas de adjacencias
+        adj = new list<pair<int, int>>[v];
     }
 
     // adiciona uma aresta ao grafo de v1 a v2
@@ -32,35 +32,32 @@ public:
         adj[v1].push_back(make_pair(v2, custo)); // tem um vertice saindo de v1 ate v2 com um custo x
     }
 
-    // algoritmo de Dijkstra
-    int dijkstra(int orig, int dest)
+    // Dijkstra retornando as distancias minimas de um vertice origem a todos os outros
+    vector<int> dijkstra(int orig)
     {
-        // vetor de distancia
         vector<int> dist;
-
-        // vetor de visistados para n repetir vertecie
         vector<int> visitados;
 
         // heap minima c++
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> heap_min; // aparti de vector é alteração que precisa ser feita para torna a heap minima
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> heap_min;
 
         // inicia o vetor de distancia e visitados
-        for (int i = 0; i < v; i++)
+        for (int i = 1; i <= v; i++) // uma vez que o grafo começa do vertice 1
         {
             dist.push_back(inf);
             visitados.push_back(false);
         }
+
         // distancia de origem para origem é 0
         dist[orig] = 0;
 
         // insere na heap
         heap_min.push(make_pair(dist[orig], orig));
 
-        // loop do algoritmo
         while (!heap_min.empty())
         {
             pair<int, int> p = heap_min.top(); // extrair o pair do topo
-            int u = p.second;                  // obtendo o vertice que é o secundo elemonto do pear o primeiro é a distancia
+            int u = p.second;                  // obtendo o vertice que é o segundo elemento do pair
             heap_min.pop();                    // removendo da fila
 
             // verifica se o vertice foi visitado
@@ -89,7 +86,7 @@ public:
             }
         }
         // retorna a distancia minima ao destino
-        return dist[dest];
+        return dist;
     }
 };
 
@@ -115,25 +112,26 @@ int read(string fileName, string modoDeOutput, string outputFileName, int v0)
     }
     catch (const char *msg)
     {
-        cerr << "Erro: " << msg << endl;
+        cerr << "\e[31mErro: " << msg << "\e[0m" << endl;
         return -1;
     }
 
-    int vertices, arestas;
     string linha;
     getline(file, linha);
 
     if (linha == "")
     {
-        cout << "Erro: Arquivo vazio!" << endl;
+        cout << "\e[31mErro: Arquivo vazio!\e[0m" << endl;
         return -1;
     }
+
+    int vertices, arestas;
 
     istringstream iss(linha);
 
     iss >> vertices >> arestas;
 
-    Grafo g(vertices);
+    Grafo g(vertices + 1); // +1 pois o grafo começa do vertice 1
 
     for (int i = 0; i < arestas; i++)
     {
@@ -146,33 +144,32 @@ int read(string fileName, string modoDeOutput, string outputFileName, int v0)
     }
 
     file.close();
+
     if (modoDeOutput == "file")
     {
         ofstream outFile;
         outFile.open(outputFileName);
 
-        for (int i = 1; i <= vertices; i++)
+        vector<int> resultado = g.dijkstra(v0);
+
+        for (int i = 1; i < resultado.size(); i++)
         {
-            outFile << i << ":";
-            int resultado = g.dijkstra(v0, i);
-            if (resultado == inf)
-                resultado = -1;
-            outFile << resultado << " ";
+            if (resultado[i] == inf)
+                resultado[i] = -1;
+            outFile << i << ":" << resultado[i] << " ";
         }
 
         outFile.close();
     }
     else
     {
-        for (int i = 1; i <= vertices; i++)
+        vector<int> resultado = g.dijkstra(v0);
+
+        for (int i = 1; i < resultado.size(); i++)
         {
-            {
-                cout << i << ":";
-                int resultado = g.dijkstra(v0, i);
-                if (resultado == inf)
-                    resultado = -1;
-                cout << resultado << " ";
-            }
+            if (resultado[i] == inf)
+                resultado[i] = -1;
+            cout << i << ":" << resultado[i] << " ";
         }
 
         cout << endl;
@@ -185,7 +182,7 @@ int main(int argc, char *argv[])
 {
     try
     {
-        bool fileRun = false;
+        bool fFlag = false, hFlag = false;
         string outputFileName = "", inputFileName = "", modoDeOutput = "terminal";
         int v0 = 1;
 
@@ -197,11 +194,12 @@ int main(int argc, char *argv[])
             if (!strcmp(argv[i], "-h"))
             {
                 help();
+                hFlag = true;
                 break;
             }
             else if ((!strcmp(argv[i], "-o") || !strcmp(argv[i], "-f") || !strcmp(argv[i], "-i")) && argv[i + 1] == NULL)
             {
-                throw "Especifique o caminho do arquivo como mostrado no help (./dijkstra -h)";
+                throw "Especifique o caminho do arquivo (ou o vértice inicial)! (Veja o help: ./dijkstra -h)";
                 break;
             }
             else if (!strcmp(argv[i], "-o"))
@@ -212,7 +210,7 @@ int main(int argc, char *argv[])
             }
             else if (!strcmp(argv[i], "-f"))
             {
-                fileRun = true;
+                fFlag = true;
                 inputFileName = argv[i + 1];
                 i++;
             }
@@ -227,11 +225,13 @@ int main(int argc, char *argv[])
                 break;
             }
         }
-        if (fileRun)
+        if (fFlag)
             read(inputFileName, modoDeOutput, outputFileName, v0);
+        else if (!hFlag)
+            throw "Nenhum arquivo de entrada foi especificado! (Veja o help: ./dijkstra -h)";
     }
     catch (const char *msg)
     {
-        cerr << "Erro: " << msg << endl;
+        cerr << "\e[31mErro: " << msg << "\e[0m" << endl;
     }
 }
